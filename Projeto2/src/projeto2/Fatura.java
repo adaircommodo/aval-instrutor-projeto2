663cbcf418c32f1d3d61ae03034f1659e367b88e
Adair José Rossatto Commodo <adaircommodo@gmail.com>
2021-02-17 16:39:24 -0300

    Criação da Classe Fatura

---
+++ Projeto2/src/projeto2/Fatura.java
@@ -0,0 +1,58 @@
+package projeto2;
+
+public class Fatura {
+	
+	private String numero;
+	private String descricao;
+	private int quantidade; 
+	private double precoPorItem;
+	
+	public Fatura(String numero, String descricao, int quantidade, double precoPorItem) {
+		this.setNumero(numero);
+		this.setDescricao(descricao);
+		this.setQuantidade(quantidade);
+		this.setPrecoPorItem(precoPorItem);
+	}
+	
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public double getPrecoPorItem() {
+		return precoPorItem;
+	}
+
+	public void setPrecoPorItem(double precoPorItem) {
+		this.precoPorItem = precoPorItem;
+	}
+
+	public double getTotalFatura() {
+		double calculo = this.getPrecoPorItem()*this.getQuantidade();
+		if(calculo<0.00){ 
+			calculo = Math.round(calculo);
+		}
+		return calculo;
+		
+	}
+	
+}
